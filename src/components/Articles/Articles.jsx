43db13cd634b630bbaa27d545d23775af9e86c27
diegoalparra <diegoalparra08@gmail.com>
2023-10-20 17:04:48 -0500

    Articles height arreglado, links en nav jusrtificados

--- src/components/Articles/Articles.jsx
+++ src/components/Articles/Articles.jsx
@@ -16,13 +16,13 @@ const Articles = () => {
                     <h4 className="mb-[14px] max-w-full text-xl font-bold text-customBlue5 md:w-[64%] md:text-3xl md:leading-[42px] lg:w-[85%] xl:w-[85%] 2xl:w-[75%] 3xl:w-[52%]">
                    Recuerda que la vacunación de los niños es muy importante
                     </h4>
-                    <p className="mb-[40px] max-w-full text-base font-medium rounded-md bg-white bg-opacity-30 p-2 text-customBlue5 md:w-[64%] lg:w-[72%] xl:w-[72%] 2xl:w-[60%] 3xl:w-[45%]">
+                    <p className="mb-[40px] max-w-full lg:h-44 xl:h-32 text-base font-medium rounded-md bg-white bg-opacity-30 p-2 text-customBlue5 md:w-[64%] lg:w-[72%] xl:w-[72%] 2xl:w-[60%] 3xl:w-[45%]">
                     La vacunación en niños protege contra enfermedades graves, previene su propagación y garantiza un futuro más saludable y seguro.
                     </p>
                     <div className="mt-[36px] flex items-center justify-between gap-4 sm:justify-start 2xl:gap-10">
                     <ButtonTwo label={'Conoce más'} />
                     <Link to='/book' className="text-customBlue5 font-medium text-lightPrimary hover:scale-105 2xl:ml-2">Agenda tu cita</Link>
-                    </div>
+                    </div> 
                 </div>
             </div>
 
@@ -34,7 +34,7 @@ const Articles = () => {
                     <h4 className="mb-[14px] max-w-full text-xl font-bold text-customBlue5 md:w-[64%] md:text-3xl md:leading-[42px] lg:w-[85%] xl:w-[85%] 2xl:w-[75%] 3xl:w-[52%]">
                     Estate atento a los signos del cáncer y hacer revisiones a tiempo
                     </h4>
-                    <p className="mb-[40px] max-w-full text-base font-medium rounded-md bg-white bg-opacity-30 p-2 text-customBlue5 md:w-[64%] lg:w-[72%] xl:w-[72%] 2xl:w-[60%] 3xl:w-[45%]">
+                    <p className="mb-[40px] max-w-full lg:h-44 xl:h-32 text-base font-medium rounded-md bg-white bg-opacity-30 p-2 text-customBlue5 md:w-[64%] lg:w-[72%] xl:w-[72%] 2xl:w-[60%] 3xl:w-[45%]">
                     Es vital estar atento a los signos del cáncer, ya que la detección temprana aumenta las posibilidades de tratamiento efectivo. Consulta a partir de los 40 años.
                     </p>
                     <div className="mt-[36px] flex items-center justify-between gap-4 sm:justify-start 2xl:gap-10">
