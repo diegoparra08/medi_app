b2d684b5d5c53a2073d5f66426b2eccf998ea62f
RSLastra <raul.slastra@gmail.com>
2023-10-23 15:35:20 -0600

    footer

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,12 +1,14 @@
 
 import {Carousel} from '../../components/Carousel/Carousel';
 import Articles from '../../components/Articles/Articles';
+import Footer from '../../components/Footer/Footer';
 
 export const Home = () => {
     return(
         <div>
             <Carousel/>
             <Articles/>
+            <Footer/>
         
         </div>
     )
