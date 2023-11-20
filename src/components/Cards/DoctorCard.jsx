ae27a182370ba5fa17af1bb91e575db185ca2c2e
diegoalparra <diegoalparra08@gmail.com>
2023-11-19 19:15:18 -0500

    vista especialidades filtra y funciona

---
+++ src/components/Cards/DoctorCard.jsx
@@ -0,0 +1,29 @@
+
+import { ButtonTwo } from "../Buttons/Buttons";
+import doc from '../../images/docprofile.jpeg'
+
+const DoctorCard = ({ name, specialties }) => {
+
+    return (
+
+        <div className="w-80 h-88 p-4 border rounded-lg shadow-md flex flex-col justify-between my-2 mx-2">
+            <div>
+                <img src={doc} alt="Doctorimg" />
+            </div>
+
+            <h2 className="text-lg font-bold">{name}</h2>
+        <ul className="p-1 space-y-1">
+            {specialties && specialties.map((specialty) => (
+                <li key={specialty.id}>{specialty.specialty_name}</li>
+            ))}
+        </ul>
+
+            <div className="mt-auto">
+                <ButtonTwo label={'Agendar Cita'} />
+            </div>
+        </div>
+
+    );
+}
+
+export default DoctorCard;
